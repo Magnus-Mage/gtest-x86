b40ac7cdf1c7cb419ffac7ac2d6c30587227221a
Magnus <104272224+Magnus-Mage@users.noreply.github.com>
2025-07-28 10:42:51 +0530

    Update: main.cpp example program

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,33 @@
 // Example file only
+
+#include <iostream>
+#include <string>
+
+// Simple utility function for your gtest-x86 library
+class AsmTestHelper {
+public:
+    static std::string getLibraryName() {
+        return "gtest-x86";
+    }
+    
+    static bool isLibraryWorking() {
+        return true;
+    }
+    
+    // Example function that could be used for assembly testing
+    static int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+    
+    // Placeholder for future assembly test utilities
+    static void logTestInfo(const std::string& message) {
+        std::cout << "[gtest-x86] " << message << std::endl;
+    }
+};
+
+// Function that can be called from tests
+extern "C" {
+    int test_library_function() {
+        return 42;
+    }
+}
